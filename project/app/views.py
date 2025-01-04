56c733bce3d7d3a139b0729dfc8a2766048c7bfe
poojasahu021994 <poojasahu02071994@gmail.com>
2025-01-04 15:55:19 +0530

    update

--- project/app/views.py
+++ project/app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def data(request):
+    print(request.methode)
+    print(request.FILES)
+    print(request.POST)
