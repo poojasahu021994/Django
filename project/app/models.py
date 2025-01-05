6decb0c002b169c124d3cf912925df975ba9cf65
poojasahu021994 <poojasahu02071994@gmail.com>
2025-01-05 19:35:30 +0530

    UPDATE

--- project/app/models.py
+++ project/app/models.py
@@ -2,3 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Student(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    detail=models.CharField(max_length=100)
+    phone=models.IntegerField(max_length=10)
+    volume=models.IntegerChoices()
+    subscribe=models.Choices()
+    gender=models.Choices()
+    dob=models.IntegerField()
+    password=models.CharField(max_length=16)
+    cpassword=models.CharField(max_length=16)
+    profile_pic=models.FilePathField()
+    resume=models.FilePathField()
--- project/app/views.py
+++ project/app/views.py
@@ -8,3 +8,81 @@ def data(request):
     print(request.methode)
     print(request.FILES)
     print(request.POST)
+
+    # name=request.POST.get('username')
+    # email=request.POST.get('email')
+    # detail=request.POST.get('detail')
+    # phone=request.POST.get('phone')
+    # age=request.POST.get('age')
+    # volume=request.POST.get('volume')
+    # subscribe=request.POST.get('subscribe')
+    # gender=request.POST.get('gender')
+    # dob=request.POST.get('dob')
+    # password=request.POST.get('password')
+    # cpassword=request.POST.get('cpassword')
+    # profile_pic=request.FILES.get('profile-pic')
+    # resume=request.FILES.get('resume')
+
+    # print(name)
+    # print(email)
+    # print(detail)
+    # print(phone)
+    # print(age)
+    # print(volume)
+    # print(subscribe)
+    # print(gender)
+    # print(dob)
+    # print(password)
+    # print(cpassword)
+    # print(profile_pic)
+    # print(resume)
+
+    # user_data={
+    #     'name' : name,
+    #     'email': email,
+    #     'detail': detail,
+    #     'phone': phone,
+    #     'age': age,
+    #     'volume': volume,
+    #     'subscribe': subscribe,
+    #     'gender': gender,
+    #     'dob': dob,
+    #     'profile_pic': profile_pic,
+    #     'resume': resume,
+    #     'password': password,
+    #     'cpassword': cpassword
+    # }
+    # print(user_data)
+
+    user_data = {
+        'name' : request.POST.get('username'),
+        'email': request.POST.get('email'),
+        'detail': request.POST.get('detail'),
+        'phone': request.POST.get('phone'),
+        'age': request.POST.get('age'),
+        'volume': request.POST.get('volume'),
+        'subscribe': request.POST.get('subscribe'),
+        'gender': request.POST.get('gender'),
+        'dob': request.POST.get('dob'),
+        'profile_pic': request.FILES.get('profile-pic'),
+        'resume': request.FILES.get('resume'),
+        'password': request.POST.get('password'),
+        'cpassword': request.POST.get('cpassword')
+       }
+    print(user_data)
+
+    # for i,j in request.POST.items():
+    #     print(i,j)
+
+    # for i in request.POST.keys():
+    #     print(i)
+
+    # for i in request.POST.values():
+    #     print(i)
+
+    # return render(request,'home.html',{'user_data':user_data})
+
+
+
+    
+
