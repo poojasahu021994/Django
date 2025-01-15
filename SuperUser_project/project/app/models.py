18fbd7412ecd26e504a6afbd876663c881e89dca
poojasahu021994 <poojasahu02071994@gmail.com>
2025-01-15 22:48:56 +0530

    change in models.py

--- SuperUser_project/project/app/models.py
+++ SuperUser_project/project/app/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
-class Student(models.Model):
+class Employee(models.Model):
+    id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=50, help_text="Enter your name")
-    email=models.EmailField(help_text="Enter Email")
+    email=models.EmailField(help_text="Enter Email", unique=True)
+    emp_joing_date =models.DateTimeField(auto_now_add=True)
+    emp_updated_at=models.DateTimeField(auto_now=True)
     detail=models.CharField(max_length=100, help_text="Enter your details")
     phone=models.IntegerField(help_text="Enter your number")
-    volume=models.IntegerField()
-    subscribe =models.CharField(max_length=10)
-    gender=models.CharField(max_length=10)
-    dob=models.DateField()
+    emp_start_time=models.TimeField()
+    gender=models.Choices(max_length=10)
+    emp_dob=models.DateTimeField()
+    emp_active=models.BooleanField()
     password=models.CharField(max_length=16)
-    profile_pic=models.ImageField()
-    resume=models.FileField()
+    profile_pic=models.ImageField(upload_to='image/')
+    resume=models.FileField(upload_to='file/')
 # Create your models here.
--- SuperUser_project/project/app/views.py
+++ SuperUser_project/project/app/views.py
@@ -1,20 +1,2 @@
 from django.shortcuts import render
-# from .models import Student
-# Create your views here.
-
-# def data(request):
-#     print(request.meth)
-
-# name = request.POST.get('username')
-# email = request.POST.get('email')
-# detail = request.POST.get('detail')
-# phone = request.POST.get('phone')
-# volume = request.POST.get('volume')
-# subscribe = request.POST.getlist('subscribe')
-# gender = request.POST.get('gender')
-# dob = request.POST.get('dob')
-# password = request.POST.get('password')
-# cpassword = request.POST.get('cpassword')
-# profile_pic = request.FILES.get('profile-pic')
-# resume = request.FILES.get('resume')
 
